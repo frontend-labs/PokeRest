e086753ee2fec6bbc9c011bbec5d12218d571ba7
Erik Flores <erik.flores@clicksandbricks.pe>
2015-09-09 10:59:38 -0500

    modules

--- test/test.js
+++ test/test.js
@@ -1,10 +1,23 @@
 var pokeServer = require('../pokeServer.js');
 var request = require('supertest')(pokeServer);
 
-describe('PokeRest', function() {
+describe('GET /', function() {
     it('returns hello world', function(done) {
         request
             .get('/')
             .expect('Hello, World!', done);
     });
+});
+
+describe('GET /listPokemon',function(){
+	it('show json',function(done){
+		request
+			.get('/listPokemon')
+			.expect('Content-Type',/json/)
+			.expect('Content-Length','20')
+			.expect(200)
+			.end(function(err, res){
+				if (err) throw err;
+			})
+	});
 });
