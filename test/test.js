02de856255269b41bcd41cc55978f99f8d93d97d
Erik Flores <erik.flores@clicksandbricks.pe>
2015-09-09 11:08:35 -0500

    update package json

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var pokeServer = require('../pokeServer.js');
 var request = require('supertest')(pokeServer);
 
-describe('GET /', function() {
+describe('index', function() {
     it('returns hello world', function(done) {
         request
             .get('/')
@@ -9,12 +9,12 @@ describe('GET /', function() {
     });
 });
 
-describe('GET /listPokemon',function(){
+describe('listPokemon',function(){
 	it('show json',function(done){
 		request
 			.get('/listPokemon')
 			.expect('Content-Type',/json/)
-			.expect('Content-Length','20')
+			.expect('Content-Length','1628')
 			.expect(200)
 			.end(function(err, res){
 				if (err) throw err;
